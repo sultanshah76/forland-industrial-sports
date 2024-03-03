62274b9fc6561bbc0762599696c668b7ffb9175f
syed <sultanqaiser835@gmail.com>
2024-03-03 15:09:05 +0500

    COMPLETED

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Resume-App",
+  title: "RESUME-APP",
   description: "Personal Portfolio",
 };
 
